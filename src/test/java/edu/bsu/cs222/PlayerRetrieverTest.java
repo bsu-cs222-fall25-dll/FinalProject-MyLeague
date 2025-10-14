0e66a11db52cb288fdfea0bc7265d34cee42b280
cburke6897 <cburke6897@gmail.com>
2025-10-14 15:23:25 -0400

    Creates PlayerRetrieverTest.getPlayersDoesNotReturnNull test

--- src/test/java/edu/bsu/cs222/PlayerRetrieverTest.java
+++ src/test/java/edu/bsu/cs222/PlayerRetrieverTest.java
@@ -1,7 +1,5 @@
 package edu.bsu.cs222;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -17,6 +15,12 @@ public class PlayerRetrieverTest {
         PlayerRetriever retriever = new PlayerRetriever();
         retriever.createPlayerList(readSampleFileAsString());
         Assertions.assertEquals("Jack Coco", retriever.getPlayerArrayList().getFirst().getName());
+    }
+
+    @Test
+    void getPlayersDoesNotReturnNull() throws InterruptedException {
+        PlayerRetriever retriever = new PlayerRetriever();
+        Assertions.assertNotNull(retriever.getPlayers());
     }
 
     private String readSampleFileAsString() throws NullPointerException, IOException {
