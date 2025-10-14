62eb3c29f426c210ce11ff850a154f409220b194
EvanWilson9 <evanpwilson1@gmail.com>
2025-10-14 16:59:49 -0400

    Adds sample.fxml file in resources folder and SampleController class. Adds scene builder plugins.

--- src/main/java/edu/bsu/cs222/GraphicalUserInterface.java
+++ src/main/java/edu/bsu/cs222/GraphicalUserInterface.java
@@ -1,60 +1,23 @@
 package edu.bsu.cs222;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-
 import java.io.IOException;
-import java.net.URISyntaxException;
+import java.util.Objects;
 
-import static javafx.application.Application.launch;
-
-public class GraphicalUserInterface {
+public class GraphicalUserInterface extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
-    private final Button retrieveButton = new Button("Retrieve");
-    private final TextField inputField = new TextField();
-    private final TextArea outputArea = new TextArea();
-
-    public void start(Stage primaryStage) {
-        configure(primaryStage);
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+        primaryStage.setTitle("MyLeague");
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
     }
-
-    private void configure(Stage stage) {
-        stage.setTitle("MyLeague");
-
-        stage.setScene(new Scene(createRoot()));
-        stage.setWidth(500);
-        stage.setMinHeight(outputArea.getHeight());
-        outputArea.setMinHeight(300);
-        stage.show();
-    }
-
-    private Pane createRoot() {
-        VBox root = new VBox();
-        root.getChildren().addAll( //
-                new Label("Input a Wikipedia Article Name: "), //
-                inputField, //
-                retrieveButton, //
-                new Label("Redirects History found:"),//
-                outputArea);
-        return root;
-    }
-
-//    private void configureRetrieveButton(){
-//        retrieveButton.setOnAction(event -> retrieveRevisions());
-//    }
 }
-
