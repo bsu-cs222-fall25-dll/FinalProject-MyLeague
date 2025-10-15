550b10107b4cba842b8d28ad1922fb6d550ab107
cburke6897 <cburke6897@gmail.com>
2025-10-15 18:17:26 -0400

    Adds team and position dropdown filters

--- src/main/java/edu/bsu/cs222/gui/controllers/PlayersViewController.java
+++ src/main/java/edu/bsu/cs222/gui/controllers/PlayersViewController.java
@@ -6,6 +6,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
 
@@ -15,12 +16,34 @@ import java.util.Arrays;
 public class PlayersViewController {
     @FXML private TextField searchField;
     @FXML private ListView<Player> listView;
+    @FXML private ComboBox<String> teamFilter;
+    @FXML private ComboBox<String> positionFilter;
     private FilteredList<Player> filteredList;
+    private final String[] positionArray = new String[] {"All", "QB"};
+    private final String[] teamArray = new String[] {"All", "ARI"};
 
     @FXML
     public void initialize() {
         listView.setFixedCellSize(70);
         listView.setCellFactory(lv -> new PlayerCell());
+
+        positionFilter.getItems().addAll(positionArray);
+        teamFilter.getItems().addAll(teamArray);
+
+        positionFilter.setValue("All");
+        teamFilter.setValue("All");
+
+        positionFilter.valueProperty().addListener((obs, oldVal, newVal) ->
+                filteredList.setPredicate(player -> {
+                    if ("All".equals(newVal)) {return true;}
+                    return player.getPosition().equalsIgnoreCase(newVal);
+                }));
+
+        teamFilter.valueProperty().addListener((obs, oldVal, newVal) ->
+                filteredList.setPredicate(player -> {
+                    if ("All".equals(newVal)) {return true;}
+                    return player.getTeam().equalsIgnoreCase(newVal);
+                }));
     }
 
     public void setPlayers(ArrayList<Player> players) {
@@ -38,8 +61,7 @@ public class PlayersViewController {
         if (player == null) {return false;}
         boolean match = true;
         for (String query : queries){
-            if (!player.getName().toLowerCase().contains(query)
-                    && !player.getPosition().toLowerCase().contains(query)) {
+            if (!player.getName().toLowerCase().contains(query)) {
                 match = false;
                 break;
             }
