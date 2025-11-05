e63936f45b7c586251be89f68815b78d2f2429f7
Oladayo Ayorinde <oladayodavid5@gmail.com>
2025-11-04 20:42:42 -0500

    Added Compare button in PlayerStatsModal.fxml to toggle player comparison panel

--- src/main/java/edu/bsu/cs222/gui/controllers/PlayerStatsModalController.java
+++ src/main/java/edu/bsu/cs222/gui/controllers/PlayerStatsModalController.java
@@ -4,15 +4,22 @@ import edu.bsu.cs222.model.Player;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.fxml.FXML;
+import javafx.scene.control.SplitPane;
+import javafx.scene.layout.VBox;
 
 public class PlayerStatsModalController {
     public Button cancelButton;
     public Button seasonStatsBtn;
     public Button weeklyStatsBtn;
+    public SplitPane splitPane;
     @FXML private Label playerLabel;
     @FXML private Label tabLbl;
+    @FXML private VBox comparePanel;
+    @FXML private Button compareButton;
+
     String currentStatView = "Season Stats";
     private Player player;
+    private boolean isComparePanelVisible = false;
 
     public void setPlayer(Player player){
         this.player = player;
@@ -36,5 +43,19 @@ public class PlayerStatsModalController {
     private void setWeeklyStatView(){
         currentStatView = "Weekly Stats";
         tabLbl.setText(currentStatView);
+    }
+
+    @FXML
+    private void toggleComparePanel() {
+        isComparePanelVisible = !isComparePanelVisible;
+        comparePanel.setVisible(isComparePanelVisible);
+        comparePanel.setManaged(isComparePanelVisible);
+
+        if (isComparePanelVisible) {
+            compareButton.setText("Hide");
+        }
+        else {
+            compareButton.setText("Compare");
+        }
     }
 }
