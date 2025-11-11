b7ab74122fc41f926e5e5c40a710dee0f325c82e
EvanWilson9 <evanpwilson1@gmail.com>
2025-11-11 14:47:16 -0500

    Creates PlayerCompareCell.fxml to display players on compare list view.

--- src/main/java/edu/bsu/cs222/gui/list_cells/PlayerCompareCell.java
+++ src/main/java/edu/bsu/cs222/gui/list_cells/PlayerCompareCell.java
@@ -1,7 +1,11 @@
 package edu.bsu.cs222.gui.list_cells;
 
 import edu.bsu.cs222.model.Player;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
 
 public class PlayerCompareCell extends ListCell<Player> {
+    public Label nameLbl;
+    public Label detailsLbl;
 }
